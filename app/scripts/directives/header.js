a24d42340288a8431bd763862672d9f73a0bd539
Erez Carmel <erez@gigaspaces.com>
2013-11-26 16:36:41 +0200

    GS-11499

--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -7,16 +7,24 @@ angular.module('cosmoUi')
                 '<div class="current-user">' +
                     '<div id="user-icon"></div> ' +
                     '<div id="user-name">{{loggedUser.name}}</div> ' +
-                    '<div id="logout-button"></div> ' +
+                    '<div id="logout-button" ng-click="logout()"></div> ' +
                 '</div> ' +
                 '<div class="search-box">' +
                     '<input type="text" id="search-field" placeholder="Search on Cloudify..">' +
-                    '<input type="button" id="search-submit">' +
+                    '<input type="button" id="search-submit" ng-click="searchCloudify()">' +
                 '</div> ',
             restrict: ' A',
-            link: function postLink(scope) {
+            link: function postLink(scope, element) {
                 scope.loggedUser = {
                     name: 'John Doe'
+                };
+
+                scope.searchCloudify = function() {
+                    console.log('search ' + element.find('#search-field').val());
+                };
+
+                scope.logout = function() {
+                    console.log('logout');
                 };
             }
         };
