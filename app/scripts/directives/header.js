61dc1366cb8a9fa1c455eab8ddb415a7250a3efd
Erez Carmel <erez@gigaspaces.com>
2013-11-26 12:00:30 +0200

    GS-11498

--- app/scripts/directives/header.js
+++ app/scripts/directives/header.js
@@ -4,10 +4,20 @@ angular.module('cosmoUi')
     .directive('header', function () {
         return {
             template: '<div class="logo"></div>' +
-                '<div class="current-user"></div> ' +
-                '<div class="search-box"></div> ',
-            restrict: ' A'//,
-        //      link: function postLink(scope, element, attrs) {
-        //      }
+                '<div class="current-user">' +
+                    '<div id="user-icon"></div> ' +
+                    '<div id="user-name">{{loggedUser.name}}</div> ' +
+                    '<div id="logout-button"></div> ' +
+                '</div> ' +
+                '<div class="search-box">' +
+                    '<input type="text" id="search-field" placeholder="Search on Cloudify..">' +
+                    '<input type="button" id="search-submit">' +
+                '</div> ',
+            restrict: ' A',
+            link: function postLink(scope) {
+                scope.loggedUser = {
+                    name: 'John Doe'
+                };
+            }
         };
     });
