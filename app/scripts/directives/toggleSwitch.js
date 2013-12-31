e513e430c9f3c829f99772130bf3d2104ee32dc7
guym <guym@gigaspaces.com>
2013-12-31 13:47:19 +0200

    fixing build.

--- app/scripts/directives/toggleSwitch.js
+++ app/scripts/directives/toggleSwitch.js
@@ -4,25 +4,25 @@ angular.module('cosmoUi')
     .directive('toggleSwitch', function () {
         return {
             template: '<div class="toggle-switch">' +
-                    '<div id="toggle-switch-text">{{text}}</div>' +
-                    '<div id="toggle-switch-button" class="{{value}}" ng-click="toggleButton()">' +
-                        '<div id="toggle-switch-button-text-on" class="toggle-switch-button-text">ON</div>' +
-                        '<div id="toggle-switch-button-circle" class="{{value}}"></div>' +
-                        '<div id="toggle-switch-button-text-off" class="toggle-switch-button-text">OFF</div>' +
-                    '</div>' +
+                '<div id="toggle-switch-text">{{text}}</div>' +
+                '<div id="toggle-switch-button" class="{{value}}" ng-click="toggleButton()">' +
+                '<div id="toggle-switch-button-text-on" class="toggle-switch-button-text">ON</div>' +
+                '<div id="toggle-switch-button-circle" class="{{value}}"></div>' +
+                '<div id="toggle-switch-button-text-off" class="toggle-switch-button-text">OFF</div>' +
+                '</div>' +
                 '</div>',
             restrict: 'EA',
             scope: {
-               text: '@',
-               value: '@'
+                text: '@',
+                value: '@'
             },
             link: function (scope, element) {
 
-                scope.toggleButton = function() {
+                scope.toggleButton = function () {
                     scope.value = scope.value === 'on' ? 'off' : 'on';
                 };
 
-                scope.$watch('value', function(newVal) {
+                scope.$watch('value', function (newVal) {
                     if (newVal === 'off') {
                         _setOff();
                     } else {
