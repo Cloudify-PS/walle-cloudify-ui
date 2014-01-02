9433edd664452b4d56198f0297f48e7c320ae143
guym <guym@gigaspaces.com>
2014-01-02 13:10:08 +0200

    fallback to memory if writing to file failed.

--- backend/gsSettings.js
+++ backend/gsSettings.js
@@ -1,15 +1,18 @@
 var fs = require('fs');
 
+var settingsObj = null;
+
 exports.read = function( req, res ) {
     var fileJSON;
     try {
         fileJSON = require('./settings.json');
-    } catch (e) {}
+    } catch (e) { return settingsObj;}
 
     return fileJSON;
 };
 
 exports.write = function( req, res ) {
+    settingsObj = req;
     fs.writeFile("backend/settings.json", JSON.stringify(req, null, 2), function(err) {
         if(err) {
             console.log(err);
