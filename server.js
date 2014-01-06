07a90ae9f44736e5d5f9bf258be641a35b6ba542
erezgiga <erez@gigaspaces.com>
2014-01-06 14:59:22 +0200

    GS-11531

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ var gsSettings = require("./backend/gsSettings");
 var conf = require("./backend/appConf");
 
 var rest = require('restler');
+var fs = require('fs');
 var http = require('http');
 var log4js = require('log4js');
 log4js.configure({
@@ -148,17 +149,42 @@ app.post('/backend/blueprints/add', function(request, response){
     var applicationFileName = request.body.application_file_name;
     var host = 'http://' + conf.cosmoServer + ':' + conf.cosmoPort + "/blueprints?application_file_name=" + applicationFileName;
 
-    rest.post(host, {
-        data: rest.file(myFile.path, myFile.name, myFile.size, null, 'application/gzip'),
-        headers: {
-            'Transfer-Encoding': 'chunked',
-            'content-type': 'application/octet-stream',
-            'Content-Encoding': 'gzip'
-        }
-    }).on('complete', function(data) {
-        logger.debug('data: ' + JSON.stringify(data));
-        response.send(200);
+//    rest.post(host, {
+//        data: rest.file(myFile.path, myFile.name, myFile.size, null, 'application/gzip'),
+//        headers: {
+//            'Transfer-Encoding': 'chunked',
+//            'content-type': 'application/octet-stream',
+//            'Content-Encoding': 'gzip'
+//        }
+//    }).on('complete', function(data) {
+//        logger.debug('data: ' + JSON.stringify(data));
+//        response.send(200);
+//    });
+
+    fs.readFile(myFile.path, function(err, data) {
+        if (err) throw err;
+        var path = '/blueprints?application_file_name=' + applicationFileName;
+        var options = {
+            hostname: conf.cosmoServer,
+            port: conf.cosmoPort,
+            path: path,
+            method: 'POST'
+        };
+
+        var req = http.request(options, function(res) {
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+        });
+
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+
+        req.write(data);
+        req.end();
     });
+
 });
 
 app.get('/backend/blueprints/get', function(request, response) {
