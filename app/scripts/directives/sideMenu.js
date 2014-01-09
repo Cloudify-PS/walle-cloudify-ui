c3d3b1fe5ee0ca036d809439dc6d6f36ee88b005
erezgiga <erez@gigaspaces.com>
2014-01-09 11:53:11 +0200

    Fixed sub-view support for the side menu

--- app/scripts/directives/sideMenu.js
+++ app/scripts/directives/sideMenu.js
@@ -7,7 +7,7 @@ angular.module('cosmoUi')
                 '<div class="menu">' +
                     '<ul>' +
                         '<li ng-repeat="item in items" ng-class="{selected:isSelected(item)}" ng-click="itemClick(item)">' +
-                            '<a href="{{item.route}}">' +
+                            '<a href="{{item.route[0]}}">' +
                                 '<div class="gs-icon-{{item.icon}} side-menu-icon"></div>' +
                                 '<div class="side-menu-title">{{item.label}}</div>' +
                             '</a>' +
@@ -23,22 +23,22 @@ angular.module('cosmoUi')
             transclude:true,
             link:function( scope ){
                 scope.items = [
-                    { 'route' : '#blueprints' ,                                     'icon' : 'plans' ,                'label':'Blueprints'},
-                    { 'route' : '#deployments' ,                                    'icon' : 'deployments' ,          'label':'Deployments'},
-                    { 'route' : '#events' ,                                         'icon' : 'events' ,               'label':'Events'},
-                    { 'route' : '#monitoring' ,                                     'icon' : 'monitoring' ,           'label':'Monitoring'},
-                    { 'route' : '#logs' ,                                           'icon' : 'logs' ,                 'label':'Logs'},
-                    { 'route' : '#hosts' ,                                          'icon' : 'hosts' ,                'label':'Hosts'},
-                    { 'route' : '#networks' ,                                       'icon' : 'networks' ,             'label':'Networks'},
-                    { 'route' : '#floating-ips' ,                                   'icon' : 'floating-ips' ,         'label':'Floating IPs'},
-                    { 'route' : '#storage' ,                                        'icon' : 'storage' ,              'label':'Storage'}
+                    { 'route' : ['#blueprints', '#blueprint'] ,                       'icon' : 'plans' ,                'label':'Blueprints'},
+                    { 'route' : ['#deployments', '#deployment'] ,                     'icon' : 'deployments' ,          'label':'Deployments'},
+                    { 'route' : ['#events'] ,                                         'icon' : 'events' ,               'label':'Events'},
+                    { 'route' : ['#monitoring'] ,                                     'icon' : 'monitoring' ,           'label':'Monitoring'},
+                    { 'route' : ['#logs'] ,                                           'icon' : 'logs' ,                 'label':'Logs'},
+                    { 'route' : ['#hosts'] ,                                          'icon' : 'hosts' ,                'label':'Hosts'},
+                    { 'route' : ['#networks'] ,                                       'icon' : 'networks' ,             'label':'Networks'},
+                    { 'route' : ['#floating-ips'] ,                                   'icon' : 'floating-ips' ,         'label':'Floating IPs'},
+                    { 'route' : ['#storage'] ,                                        'icon' : 'storage' ,              'label':'Storage'}
                 ];
 
                 scope.selectedItem = null;
 
                 scope.isSelected = function(item) {
-
-                    return $location.path().substr(1 ) === item.route.substr(1).split('?')[0];
+                    return item.route.indexOf('#' + $location.path().substr(1)) >= 0;
+//                    return $location.path().substr(1) === item.route.substr(1).split('?')[0];
                 };
 
                 scope.itemClick = function(item) {
