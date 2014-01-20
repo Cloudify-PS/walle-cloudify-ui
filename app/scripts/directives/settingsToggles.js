e4980403e1ebf31c3fb31347232fe2a2c5391b78
erezgiga <erez@gigaspaces.com>
2014-01-20 15:06:21 +0200

    GS-11565

---
+++ app/scripts/directives/settingsToggles.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('cosmoUi')
+    .directive('settingsToggles', function () {
+        return {
+            template: '<div class="settings-toggles">' +
+                    '<div id="settings-toggles-button" ng-click="toggleOpenList()" ng-class="{bordered: isOpen()}"></div>' +
+                    '<div id="settings-toggles-container" ng-show="isOpen()">' +
+                        '<div id="settings-toggles-title">Topology settings</div>' +
+                        '<ul ng-repeat="toggle in toggles">' +
+                            '<li><div class="settings-toggle-switch" toggle-switch text="{{toggle.name}}" value="{{toggle.state}}" ng-click="updateToggle(toggle.name)"></div></li>' +
+                        '</ul>' +
+                    '</div>' +
+                '</div>',
+            restrict: 'EA',
+            scope: {
+                'toggles': '='
+            },
+            link: function (scope) {
+                var isOpen = false;
+
+                scope.toggleOpenList = function() {
+                    isOpen = !isOpen;
+                };
+
+                scope.isOpen = function() {
+                    return isOpen;
+                };
+
+                scope.updateToggle = function(toggleName) {
+                    for (var i in scope.toggles) {
+                        var toggle = scope.toggles[i];
+                        if (toggle.name === toggleName) {
+                            toggle.state = !toggle.state;
+                        }
+                    }
+                };
+            }
+        };
+    });
---
+++ test/spec/directives/settingsToggles.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Directive: settingsToggles', function () {
+
+//  // load the directive's module
+//  beforeEach(module('cosmoUiApp'));
+//
+//  var element,
+//    scope;
+//
+//  beforeEach(inject(function ($rootScope) {
+//    scope = $rootScope.$new();
+//  }));
+});
