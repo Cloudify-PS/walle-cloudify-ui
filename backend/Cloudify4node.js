0a178fde6830310092caa88fd379760c78d58e92
erezgiga <erez@gigaspaces.com>
2014-01-28 14:19:43 +0200

    GS-11586

--- backend/Cloudify4node.js
+++ backend/Cloudify4node.js
@@ -2,12 +2,7 @@ var ajax = require("http");
 var fs = require('fs');
 var conf = require("../backend/appConf");
 var log4js = require('log4js');
-log4js.configure({
-    appenders: [
-        { type: 'console' },
-        { type: 'file', filename: 'logs/gsui.log', category: 'gsui' }
-    ]
-});
+log4js.configure(conf.loggerConfig);
 var logger = log4js.getLogger('server');
 
 module.exports = Cloudify4node;
