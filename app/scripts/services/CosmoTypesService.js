7fbc4be51dc733dfe1e845189d6f816555d1d4d5
erezgiga <erez@gigaspaces.com>
2014-03-19 16:21:26 +0200

    CFY-440

---
+++ app/scripts/services/CosmoTypesService.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('cosmoUi')
+    .service('Cosmotypesservice', function Cosmotypesservice() {
+        var typeData = [
+            {
+                name: 'typeName',
+                baseType: 'baseType'
+            }
+        ];
+    });
