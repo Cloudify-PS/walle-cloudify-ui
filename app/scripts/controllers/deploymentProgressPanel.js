70fdcfffeeb7492c35634b27d2422bd66ef0c584
erezgiga <erez@gigaspaces.com>
2014-05-11 14:53:36 +0300

    CFY-634

--- app/scripts/controllers/deploymentProgressPanel.js
+++ app/scripts/controllers/deploymentProgressPanel.js
@@ -3,7 +3,44 @@
 angular.module('cosmoUi')
     .controller('DeploymentProgressPanelCtrl', function ($scope) {
         $scope.panelOpen = false;
-        //$scope.nodes = [];
+        $scope.nodes = [
+            {
+                name: 'Node112233',
+                inProgress: 5,
+                ready: 0,
+                failed: 0,
+                status: 'in progress'
+            },
+            {
+                name: 'Node112234',
+                inProgress: 5,
+                ready: 0,
+                failed: 0,
+                status: 'success'
+            },
+            {
+                name: 'Node112235',
+                inProgress: 5,
+                ready: 0,
+                failed: 0,
+                status: 'failed'
+            },
+            {
+                name: 'Node112236',
+                inProgress: 5,
+                ready: 0,
+                failed: 0,
+                status: 'N/A'
+            }
+        ];
+
+        $scope.getWorkflow = function() {
+            if ($scope.selectedWorkflow.data === null) {
+                return 'All workflows'
+            } else {
+                return $scope.selectedWorkflow.data.label;
+            }
+        };
 
         $scope.togglePanel = function() {
             $scope.panelOpen = $scope.panelOpen === false;
