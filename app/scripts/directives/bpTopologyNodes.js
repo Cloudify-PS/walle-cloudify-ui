10f72af2401a582e17e70cef9336fed613c75ee3
erezgiga <erez@gigaspaces.com>
2014-06-03 12:53:29 +0300

    CFY-631

--- app/scripts/directives/bpTopologyNodes.js
+++ app/scripts/directives/bpTopologyNodes.js
@@ -9,21 +9,9 @@ angular.module('cosmoUi')
             scope: {
                 map: '='
             },
-            link: function (scope) {
-                scope.myvar = 'hello';
-//                scope.getContainerClass = function(node) {
-//                    return node;
-//                };
-
-                scope.getNodeContainerClass = function(node) {
-                    if (node.children !== undefined && node.children.length > 0) {
-                        return 'box';
-                    }
-                    if (node.children === undefined) {
-                        return 'app';
-                    }
-                };
-            },
+//            link: function (scope) {
+//
+//            },
             compile: function(element) {
                 return RecursionHelper.compile(element);
             }
