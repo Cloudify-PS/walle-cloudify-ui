9991c6c6cd8bdc3e1f611ca37f10ed00f5b609af
erezgiga <erez@gigaspaces.com>
2014-08-10 18:47:00 +0300

    CFY-1008

--- test/spec/directives/header.js
+++ test/spec/directives/header.js
@@ -2,26 +2,16 @@
 
 describe('Directive: header', function () {
 
-    //load the directive's module
     beforeEach(module('cosmoUiApp'));
 
     var element, scope;
 
     beforeEach(inject(function ($compile, $rootScope) {
         scope = $rootScope.$new();
-        element = $compile('<div header></div>')(scope);
-//
-//        element = angular.element("<test header></test>");
-//
-//        scope = $rootScope;
-//        scope.defined = false;
-//
-//        $compile(element)(scope);
-//        scope.$digest();
+        element = $compile(angular.element('<div header></div>'))(scope);
     }));
 
     it('should create an element', function() {
 //        expect(element).not.toBeUndefined();
-//        console.log(element);
     });
 });
