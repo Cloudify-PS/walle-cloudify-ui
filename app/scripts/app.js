3c7dfae5b09efd4644c0d6b183d3e02fad1311b0
erezgiga <erez@gigaspaces.com>
2014-08-10 17:12:39 +0300

    CFY-1008

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -76,7 +76,8 @@ angular.module('cosmoUiApp', [
     .value('appConfig', {
         versions: {
             ui: '0.0',
-            manager: '0.0'
+            manager: '0.0',
+            latest: ''
         }
     })
     .run(['I18next', 'RestService', '$log', 'appConfig', function(I18next, RestService, $log, appConfig) {
@@ -96,7 +97,9 @@ angular.module('cosmoUiApp', [
             .then(function(data){
                 if(data.hasOwnProperty('version')) {
                     appConfig.versions.ui = data.version;
-                    RestService.checkVersion(data.version);
+                    RestService.checkVersion(data.version.split('.').join('')).then(function(ver) {
+                        appConfig.versions.latest = ver;
+                    });
                 }
             });
 
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ var log4js = require('log4js');
 var logger = log4js.getLogger('server');
 var influx = require('influx');
 var fs = require('fs');
+var http = require('http');
 
 fs.mkdir('logs', function(e) {
     if (!e) {
@@ -312,11 +313,13 @@ app.get('/backend/configuration', function (request, response) {
 
 app.get('/backend/version/check', function(request, response) {
     var currentVersion = request.query.version;
-//    http.get('http://www.getcloudify.org/latestversion?version=' + currentVersion, function(res) {
-//        response.send(res);
-//    }).on('error', function(e) {
+    http.get('http://www.gigaspaces.com/downloadgen/latest-cloudify-version?build=' + currentVersion, function(res) {
+        res.on("data", function(chunk) {
+            response.send(chunk);
+        });
+    }).on('error', function(e) {
 //        response.send('Got error: ' + e.message);
-//    });
+    });
 });
 
 
