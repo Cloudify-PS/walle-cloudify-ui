04922c0ba4f0c7aa953eb0591f19b30f156c8bb7
erezgiga <erez@gigaspaces.com>
2014-08-14 14:33:32 +0300

    CFY-1034

--- test/spec/controllers/FileSelectionDialog.js
+++ test/spec/controllers/FileSelectionDialog.js
@@ -2,18 +2,34 @@
 
 describe('Controller: FileSelectionDialogCtrl', function () {
 
-//  // load the controller's module
-//  beforeEach(module('cosmoUiApp'));
-//
-//  var PopupCtrl,
-//    scope;
-//
-//  // Initialize the controller and a mock scope
-//  beforeEach(inject(function ($controller, $rootScope) {
-//    scope = $rootScope.$new();
-//    PopupCtrl = $controller('PopupCtrl', {
-//      $scope: scope
-//    });
-//  }));
+    var FileSelectionDialogCtrl, scope;
 
+    // load the controller's module
+    beforeEach(module('cosmoUiApp', 'ngMock'));
+
+    // Initialize the controller and a mock scope
+    describe('Test setup', function() {
+        it ('', inject(function ($controller, $rootScope, $httpBackend) {
+            $httpBackend.whenGET("/backend/configuration?access=all").respond(200);
+            $httpBackend.whenGET("/backend/versions/ui").respond(200);
+            $httpBackend.whenGET("/backend/versions/manager").respond(200);
+            $httpBackend.whenGET("/backend/version/latest?version=00").respond('300');
+
+            scope = $rootScope.$new();
+
+            FileSelectionDialogCtrl = $controller('FileSelectionDialogCtrl', {
+                $scope: scope
+            });
+        }));
+    });
+
+    describe('Controller tests', function() {
+        it('should create a controller', function () {
+            expect(FileSelectionDialogCtrl).not.toBeUndefined();
+        });
+
+        it('should require a blueprint name', function() {
+
+        });
+    });
 });
