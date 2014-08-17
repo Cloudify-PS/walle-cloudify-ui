66e44bb59886d349add44be70ebba9f7ada6ef29
erezgiga <erez@gigaspaces.com>
2014-08-17 17:51:35 +0300

    CFY-921

--- test/backend/unit/jasmine/tempFolder.spec.js
+++ test/backend/unit/jasmine/tempFolder.spec.js
@@ -2,12 +2,12 @@
 
 describe('Backend: tempFolder', function () {
     var os = require('os');
-    var conf = require("../appConf");
+    var conf = require('../../../../backend/appConf.js');
 
     it('should return system folder for temporary content', function() {
         var systemTempFolder = os.tmpdir();
         var backendTempFolder = conf.browseBlueprint.path;
 
-        expect(backendTempFolder.indexOf(systemTempFolder)).toBe(1);
+        expect(backendTempFolder.indexOf(systemTempFolder)).toBe(0);
     });
 });
