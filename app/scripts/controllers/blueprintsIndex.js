a231a065ca1cda48826f84b4f5bb9e42c1554fc4
erezgiga <erez@gigaspaces.com>
2014-08-20 13:33:09 +0300

    CFY-1114

--- app/scripts/controllers/blueprintsIndex.js
+++ app/scripts/controllers/blueprintsIndex.js
@@ -3,11 +3,13 @@
 angular.module('cosmoUiApp')
     .controller('BlueprintsIndexCtrl', function ($scope, $location, $cookieStore, RestService, BreadcrumbsService, $timeout, $log) {
         $scope.isAddDialogVisible = false;
-        $scope.isDeployDialogVisible = false;
+        $scope.isDeployDialogVisible = false
+        $scope.isDeleteBlueprintVisible = false;
         $scope.lastExecutedPlan = null;
         $scope.selectedBlueprint = null;
         var _blueprintsArr = [];
         var cosmoError = false;
+        var currentBlueprintToDelete = null;
 
         BreadcrumbsService.push('blueprints',
             {
@@ -28,6 +30,21 @@ angular.module('cosmoUiApp')
         $scope.toggleDeployDialog = function(blueprint) {
             $scope.selectedBlueprint = blueprint || null;
             $scope.isDeployDialogVisible = $scope.isDeployDialogVisible === false;
+        };
+
+        $scope.toggleDeleteDialog = function() {
+            $scope.isDeleteBlueprintVisible = $scope.isDeleteBlueprintVisible === false;
+        }
+
+        $scope.deleteBlueprint = function(blueprint) {
+            currentBlueprintToDelete = blueprint;
+            $scope.delBlueprintName = blueprint.id;
+            $scope.delBlueprintError = false;
+            $scope.toggleDeleteDialog();
+        };
+
+        $scope.confirmDeleteBlueprint = function() {
+            _deleteBlueprint();
         };
 
         $scope.loadBlueprints = function() {
@@ -95,6 +112,12 @@ angular.module('cosmoUiApp')
             }
 
             return nextIndex;
+        }
+
+        function _deleteBlueprint() {
+            if(currentBlueprintToDelete !== null) {
+                console.log('currentBlueprintToDelete', currentBlueprintToDelete.id);
+            }
         }
 
         $scope.loadBlueprints();
