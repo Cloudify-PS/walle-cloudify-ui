a1c513808dbcbaf17bc894275404cb25deac6301
erezgiga <erez@gigaspaces.com>
2014-09-01 22:36:08 +0300

    CFY-1249

--- app/scripts/controllers/deployment.js
+++ app/scripts/controllers/deployment.js
@@ -48,6 +48,7 @@ angular.module('cosmoUiApp')
         $scope.workflowsList = [];
         $scope.currentExecution = null;
         $scope.executedErr = false;
+        $scope.executionsList = [];
 
         var id = $routeParams.id;
         var blueprint_id = $routeParams.blueprint_id;
@@ -513,8 +514,9 @@ angular.module('cosmoUiApp')
                     // Execution
                     RestService.autoPull('getDeploymentExecutions', id, RestService.getDeploymentExecutions)
                         .then(null, null, function (dataExec) {
-                            $log.info('data exec', dataExec);
+//                            $log.info('data exec', dataExec);
                             if (dataExec.length > 0) {
+                                $scope.executionsList = dataExec;
                                 $scope.currentExecution = _getCurrentExecution(dataExec);
                                 $log.info('current execution is', $scope.currentExecution, $scope.deploymentInProgress  );
                                 if ( !$scope.currentExecution && $scope.deploymentInProgress) { // get info for the first time
