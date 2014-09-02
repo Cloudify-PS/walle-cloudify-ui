7c388c1f54480d0fbe4a07946cfe014ce4aa52b8
erezgiga <erez@gigaspaces.com>
2014-09-02 17:45:13 +0300

    CFY-1249

--- test/spec/filters/dateFormat.spec.js
+++ test/spec/filters/dateFormat.spec.js
@@ -9,7 +9,12 @@ describe('Filter: dateFormat', function () {
             module('cosmoUiApp', 'ngMock');
 
             // initialize a new instance of the filter
-            inject(function ($filter) {
+            inject(function ($filter, $httpBackend) {
+                $httpBackend.whenGET("/backend/configuration?access=all").respond(200);
+                $httpBackend.whenGET("/backend/versions/ui").respond(200);
+                $httpBackend.whenGET("/backend/versions/manager").respond(200);
+                $httpBackend.whenGET("/backend/version/latest?version=00").respond('300');
+
                 dateFormat = $filter('dateFormat');
             });
         });
