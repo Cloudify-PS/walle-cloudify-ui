a8bf7185a9523bd941b204ab61c5e0f603baf631
erezgiga <erez@gigaspaces.com>
2014-09-09 17:03:57 +0300

    CFY-1263

--- test/spec/controllers/logs.js
+++ test/spec/controllers/logs.js
@@ -1,22 +1,30 @@
 'use strict';
 
 describe('Controller: LogsCtrl', function () {
+    var LogsCtrl, scope;
 
-  // load the controller's module
-//  beforeEach(module('cosmoUiApp'));
-//
-//  var LogsCtrl,
-//    scope;
-//
-//  // Initialize the controller and a mock scope
-//  beforeEach(inject(function ($controller, $rootScope) {
-//    scope = $rootScope.$new();
-//    LogsCtrl = $controller('LogsCtrl', {
-//      $scope: scope
-//    });
-//  }));
-//
-//  it('should attach a list of awesomeThings to the scope', function () {
-//    expect(scope.awesomeThings.length).toBe(3);
-//  });
+    // load the controller's module
+    beforeEach(module('cosmoUiApp', 'ngMock'));
+
+    describe('Test setup', function() {
+        it ('', inject(function ($controller, $rootScope, $httpBackend) {
+            $httpBackend.whenGET("/backend/configuration?access=all").respond(200);
+            $httpBackend.whenGET("/backend/versions/ui").respond(200);
+            $httpBackend.whenGET("/backend/versions/manager").respond(200);
+            $httpBackend.whenGET("/backend/version/latest?version=00").respond('300');
+
+            scope = $rootScope.$new();
+
+            LogsCtrl = $controller('LogsCtrl', {
+                $scope: scope
+            });
+        }));
+    });
+
+    describe('Controller tests', function() {
+
+        it('should create a controller', function () {
+            expect(LogsCtrl).not.toBeUndefined();
+        });
+    });
 });
