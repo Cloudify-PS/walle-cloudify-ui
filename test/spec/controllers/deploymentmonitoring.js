caed342200871cc72e677af6075080a907e7eb62
itsikm <itsik@gigaspaces.com>
2014-09-14 13:26:40 +0300

    CFY-1191

---
+++ test/spec/controllers/deploymentmonitoring.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('Controller: DeploymentmonitoringCtrl', function () {
+
+//  // load the controller's module
+//  beforeEach(module('cosmoUiAppApp'));
+//
+//  var DeploymentmonitoringCtrl,
+//    scope;
+//
+//  // Initialize the controller and a mock scope
+//  beforeEach(inject(function ($controller, $rootScope) {
+//    scope = $rootScope.$new();
+//    DeploymentmonitoringCtrl = $controller('DeploymentmonitoringCtrl', {
+//      $scope: scope
+//    });
+//  }));
+//
+//  it('should attach a list of awesomeThings to the scope', function () {
+//    expect(scope.awesomeThings.length).toBe(3);
+//  });
+});
---
+++ test/spec/services/deploymentevents.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: deploymentEvents', function () {
+//
+//  // load the service's module
+//  beforeEach(module('cosmoUiAppApp'));
+//
+//  // instantiate service
+//  var deploymentEvents;
+//  beforeEach(inject(function (_deploymentEvents_) {
+//    deploymentEvents = _deploymentEvents_;
+//  }));
+//
+//  it('should do something', function () {
+//    expect(!!deploymentEvents).toBe(true);
+//  });
+
+});
