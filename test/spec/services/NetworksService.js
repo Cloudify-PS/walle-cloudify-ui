31021a89f32cd8fba2707c4498718a65ee189410
itsikm <itsik@gigaspaces.com>
2014-10-01 11:54:36 +0300

    CFY-1389

---
+++ test/spec/services/NetworksService.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: NetworksService', function () {
+
+//    // load the service's module
+//    beforeEach(module('cosmoUiApp'));
+//
+//    // instantiate service
+//    var NetworksService;
+//    beforeEach(inject(function (_NetworksService_) {
+//        NetworksService = _NetworksService_;
+//    }));
+//
+//    it('should do something', function () {
+//        expect(!!NetworksService).toBe(true);
+//    });
+
+});
