d0298145f38c5ba8ba1205f85c7ab4aeb6480ae2
itsikm <itsik@gigaspaces.com>
2014-10-02 10:15:08 +0300

    CFY-1389

--- test/spec/services/breadcrumbsService.js
+++ test/spec/services/breadcrumbsService.js
@@ -2,17 +2,28 @@
 
 describe('Service: Breadcrumbsservice', function () {
 
-//  // load the service's module
-//  beforeEach(module('cosmoUiApp'));
-//
-//  // instantiate service
-//  var Breadcrumbsservice;
-//  beforeEach(inject(function (_Breadcrumbsservice_) {
-//    Breadcrumbsservice = _Breadcrumbsservice_;
-//  }));
-//
-//  it('should do something', function () {
-//    expect(!!Breadcrumbsservice).toBe(true);
-//  });
+    var Breadcrumbsservice;
+
+    describe('Test setup', function() {
+        it('Injecting required data & initializing a new instance', function() {
+
+            // Load the app module
+            module('cosmoUiApp');
+
+            // Initialize a new instance of Breadcrumbsservice
+            inject(function (_Breadcrumbsservice_) {
+                Breadcrumbsservice = _Breadcrumbsservice_;
+            });
+
+        });
+    });
+
+    describe('Unit tests', function() {
+
+        it('should create a new Breadcrumbsservice instance', function() {
+            expect(Breadcrumbsservice).not.toBeUndefined();
+        });
+
+    });
 
 });
