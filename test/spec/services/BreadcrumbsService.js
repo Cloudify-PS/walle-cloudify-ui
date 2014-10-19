8815f038aa39534ba69e719450c43ad710109b42
itsikm <itsik@gigaspaces.com>
2014-10-19 10:20:53 +0300

    CFY-1175

---
+++ test/spec/services/BreadcrumbsService.js
@@ -0,0 +1,29 @@
+'use strict';
+
+describe('Service: BreadcrumbsService', function () {
+
+    var BreadcrumbsService;
+
+    describe('Test setup', function() {
+        it('Injecting required data & initializing a new instance', function() {
+
+            // Load the app module
+            module('cosmoUiApp', 'gsUiHelper');
+
+            // Initialize a new instance of BreadcrumbsService
+            inject(function (_BreadcrumbsService_) {
+                BreadcrumbsService = _BreadcrumbsService_;
+            });
+
+        });
+    });
+
+    describe('Unit tests', function() {
+
+        it('should create a new BreadcrumbsService instance', function() {
+            expect(BreadcrumbsService).not.toBeUndefined();
+        });
+
+    });
+
+});
