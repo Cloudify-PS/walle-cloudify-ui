7abf2b495200a9e8b65bf2a726da7177f8199b06
erezgiga <erez@tikalk.com>
2014-10-20 16:43:01 +0300

    CFY-1450

--- karma.conf.js
+++ karma.conf.js
@@ -75,7 +75,7 @@ module.exports = function(config) {
         // - PhantomJS
         // - IE (only Windows)
         // CLI --browsers Chrome,Firefox,Safari
-        browsers: ['PhantomJs'],
+        browsers: ['Chrome'],
 
         // If browser does not capture in given timeout [ms], kill it
         // CLI --capture-timeout 5000
