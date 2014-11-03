e274e7a8fa93df3e6b4af3f97812e31dc3970f82
itsikm <itsik@gigaspaces.com>
2014-11-03 15:08:09 +0200

    CFY-1288 - breadcrumbs modification

--- app/scripts/directives/deploymentLayout.js
+++ app/scripts/directives/deploymentLayout.js
@@ -77,14 +77,18 @@ angular.module('cosmoUiApp')
 
                         // Add breadcrumbs for the current deployment
                         $scope.breadcrumb = [
+//                            {
+//                                href: '#/blueprint/' + dataDeployment.blueprint_id + '/topology',
+//                                label: dataDeployment.blueprint_id,
+//                                id: 'blueprint'
+//                            },
                             {
-                                href: '#/blueprint/' + dataDeployment.blueprint_id + '/topology',
-                                label: dataDeployment.blueprint_id,
-                                id: 'blueprint'
-                            },
-                            {
-                                href: '',
+                                href: false,
                                 label: dataDeployment.id,
+                                brackets: {
+                                    label: dataDeployment.blueprint_id,
+                                    href: '#/blueprint/' + dataDeployment.blueprint_id + '/topology'
+                                },
                                 id: 'deployment'
                             }
                         ];
