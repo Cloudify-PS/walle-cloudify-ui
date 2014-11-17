253536f2221ebd4315624866b1b7d84880747b91
itsikm <itsik@gigaspaces.com>
2014-11-17 17:47:54 +0200

    CFY-1634 - Hosts not working properly - fixed

--- app/scripts/services/NodeSearchService.js
+++ app/scripts/services/NodeSearchService.js
@@ -13,7 +13,6 @@ angular.module('cosmoUiApp')
         var blueprintsList = [];
         var deploymentList = [];
         var nodesList = [];
-        var existingNodes = [];
 
         (function _initData() {
             CloudifyService.blueprints.list()
@@ -41,53 +40,111 @@ angular.module('cosmoUiApp')
             return nodesList;
         }
 
-        function _execute(type, filter, blueprint_id) {
+        function _getDeploymentsByBlueprintId(id) {
+            var deployments = [];
+            for(var i in deploymentList) {
+                if(deploymentList[i].parent === id) {
+                    deployments.push(deploymentList[i]);
+                }
+            }
+            return deployments;
+        }
+
+        function _getDeploymentsNodes(deployments) {
             var deferred = $q.defer();
+            var result = [];
+            var count = 0;
 
-            nodesList = [];
-            existingNodes = [];
-            deploymentList.forEach(function(deployment){
-                CloudifyService.getNodes({deployment_id: deployment.value})
-                    .then(function(nodes) {
-                        var _loadMethod;
-                        if (filter.deployment_id !== undefined) {
-                            _loadMethod = CloudifyService.deployments.getDeploymentNodes(filter);
+            function getNodes(deploymentId) {
+                CloudifyService.getNodes({deployment_id: deploymentId})
+                    .then(function(nodes){
+                        count++;
+                        result = result.concat(nodes);
+
+                        if(count === deployments.length) {
+                            deferred.resolve(result);
                         }
-                        else {
-                            _loadMethod = CloudifyService.getNodeInstances(filter);
-                        }
-                        _loadMethod.then(function (instances) {
-                            if(instances instanceof Array) {
-                                instances.forEach(function(instance) {
-                                    if(nodes instanceof Array) {
-                                        nodes.forEach(function (node) {
-                                            if (instance.node_id === node.id && node.type_hierarchy.join(',').indexOf(type) > -1) {
-                                                if(existingNodes.indexOf(instance.id) === -1) {
-                                                    if(!blueprint_id) {
-                                                        nodesList.push(instance);
-                                                    }
-                                                    else if(blueprint_id === node.blueprint_id) {
-                                                        nodesList.push(instance);
-                                                    }
-                                                    existingNodes.push(instance.id);
-                                                }
-                                            }
-                                        });
-                                    }
-                                });
-                            }
-                            deferred.resolve(nodesList);
-                        });
                     });
-            });
+            }
 
+            for(var i in deployments) {
+                getNodes(deployments[i].value);
+            }
             return deferred.promise;
         }
 
+        function _getDeploymentNodesInstances(deployments) {
+            var deferred = $q.defer();
+            var result = [];
+            var count = 0;
+
+            function getDeploymentNodes(deploymentId) {
+                CloudifyService.deployments.getDeploymentNodes({deployment_id: deploymentId})
+                    .then(function(nodes){
+                        count++;
+                        result = result.concat(nodes);
+
+                        if(count === deployments.length) {
+                            deferred.resolve(result);
+                        }
+                    });
+            }
+
+            for(var i in deployments) {
+                getDeploymentNodes(deployments[i].value);
+            }
+            return deferred.promise;
+        }
+
+        function _filterNodesByType(type, nodesInstances, nodes) {
+            var filterNodesInstances = [];
+
+            for(var i in nodesInstances) {
+                var instance = nodesInstances[i];
+                var node = _findNodeById(instance.node_id, nodes);
+
+                if(node && node.type_hierarchy.join(',').indexOf(type) > -1) {
+                    filterNodesInstances.push(instance);
+                }
+            }
+
+            return $q.all(filterNodesInstances);
+        }
+
+        function _findNodeById(id, nodes) {
+            for(var i in nodes) {
+                if(nodes[i].id === id) {
+                    return nodes[i];
+                }
+            }
+            return null;
+        }
+
+        function _execute(type, blueprintId, deployments) {
+            var deferred = $q.defer();
+
+            if(!deployments.length) {
+                deployments = _getDeploymentsByBlueprintId(blueprintId);
+            }
+
+            _getDeploymentsNodes(deployments)
+                .then(function(nodes){
+
+                    _getDeploymentNodesInstances(deployments)
+                        .then(function(instances){
+
+                            _filterNodesByType(type, instances, nodes)
+                                .then(function(instances){
+                                    deferred.resolve(instances);
+                                });
+                        });
+                });
+
+            return deferred.promise;
+        }
 
         this.getBlueprints = _getBlueprints;
         this.getDeployments = _getDeployments;
         this.getNodesList = _getNodesList;
         this.execute = _execute;
-
     });
