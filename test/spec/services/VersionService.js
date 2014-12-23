9501ff2c9a8aa74a5f80106a518fc5dd585a2e23
erezgiga <erez@gigaspaces.com>
2014-12-23 14:09:57 +0200

    CFY-1805 Added version service to handle versions (manager/ui) loading, handling multiple version loading requests, and caching the first version response from current session for further version loading requests.

--- test/spec/services/VersionService.js
+++ test/spec/services/VersionService.js
@@ -1,40 +1,53 @@
 'use strict';
 
 describe('Service: VersionService', function () {
-    var versionService;
+    var versionService, httpBackend;
 
     describe('Test setup', function() {
         it('Injecting required data & initializing a new instance', function() {
 
             // load the filter's module
-            module('cosmoUiApp', 'ngMock', 'gsUiHelper');
+            module('cosmoUiApp', 'ngMock');
 
             // initialize a new instance of the filter
-            inject(function (VersionService) {
+            inject(function (VersionService, $httpBackend) {
+                httpBackend = $httpBackend;
+                httpBackend.whenGET("/backend/configuration?access=all").respond(200);
+                httpBackend.whenGET("/backend/version/latest?version=00").respond('300');
+
                 versionService = VersionService;
             });
         });
     });
 
     describe('Unit tests', function() {
+
         it('should create a new VersionService instance', function() {
             expect(versionService).not.toBeUndefined();
         });
 
-        xit('should call backend for ui version', inject(function($http) {
-            spyOn($http, 'get').andCallThrough();
+        it('should call backend for ui version', function() {
+            var url = '/backend/versions/ui';
+
+            httpBackend.whenGET("/backend/versions/manager").respond(200);
+            httpBackend.whenGET(url).respond(200, '310');
+            httpBackend.expectGET(url);
 
             versionService.getUiVersion();
 
-            expect($http.get).toHaveBeenCalled();
-        }));
+            httpBackend.flush();
+        });
 
-        xit('should call backend for manager version', inject(function($http) {
-            spyOn($http, 'get').andCallThrough();
+        it('should call backend for manager version', function() {
+            var url = '/backend/versions/manager';
+
+            httpBackend.whenGET("/backend/versions/ui").respond(200);
+            httpBackend.whenGET(url).respond(200, '310');
+            httpBackend.expectGET(url);
 
             versionService.getManagerVersion();
 
-            expect($http.get).toHaveBeenCalled();
-        }));
+            httpBackend.flush();
+        });
     });
 });
