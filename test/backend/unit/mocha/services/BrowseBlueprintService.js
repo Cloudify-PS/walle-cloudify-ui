ab82ac9e42cb8039a6602a4dfe01082a871afcd1
erezgiga <metallica>
2015-01-20 12:13:14 +0200

    CFY-1496 added BrowseBlueprint service backend test for empty sources folder

--- test/backend/unit/mocha/services/BrowseBlueprintService.js
+++ test/backend/unit/mocha/services/BrowseBlueprintService.js
@@ -2,18 +2,36 @@
 var expect = require('expect.js');
 var conf = require('../../../../../backend/appConf');
 var path = require('path');
+var fs = require('fs');
 var BrowseBlueprintService = require('../../../../../backend/services/BrowseBlueprintService');
 
 
 describe('BrowseBlueprintService', function() {
 
-    describe('browseBlueprint', function() {
+    function setBlueprintsPath() {
         conf.browseBlueprint.path = path.join(__dirname, '../../../resources/');
+    }
 
+    function removeEmptyFolders() {
+        fs.rmdir(conf.browseBlueprint.path + '/invalidBlueprint/111', function() {
+            fs.rmdir(conf.browseBlueprint.path + '/invalidBlueprint/');
+        });
+    }
+
+    describe('browseBlueprint', function() {
         it('should return error if source folder empty', function() {
-            BrowseBlueprintService.browseBlueprint('invalidBlueprint', '111', function(err) {
-                expect(err.message).to.be('Error browsing blueprint files');
+            setBlueprintsPath();
+
+            fs.mkdir(conf.browseBlueprint.path + 'invalidBlueprint/', function() {
+                fs.mkdir(conf.browseBlueprint.path + 'invalidBlueprint/111', function() {
+                    BrowseBlueprintService.browseBlueprint('invalidBlueprint', '111', function(err) {
+                        removeEmptyFolders();
+
+                        expect(err.message).to.be('Error browsing blueprint files');
+                    });
+                });
             });
+
         });
     });
 });
