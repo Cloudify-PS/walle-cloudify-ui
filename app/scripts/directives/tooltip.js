a146aa44e0b7f62e1a54317a7893f8fb9c242f64
erezgiga <metallica>
2015-03-03 11:32:31 +0200

    CFY-2198 Added tooltip attribute directive to dynamically add title attribute to headers

--- app/scripts/directives/tooltip.js
+++ app/scripts/directives/tooltip.js
@@ -6,8 +6,7 @@ angular.module('cosmoUiApp')
             restrict: 'A',
             link: function postLink(scope, element) {
                 scope.$watch(function () {
-                    console.log(element.prop("tagName"), element);
-                    return element.html();
+                    return element.text();
                 }, function (val) {
                     element.attr('title', val.replace(/\s/g, ''));
                 });
