40f7c77f4f93200845d54488e021a69a5c788dfa
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-04-05 22:37:25 +0300

    fix (events): proper drag event panel
    closes CFY-2470

--- test/spec/directives/deploymentevents.js
+++ test/spec/directives/deploymentevents.js
@@ -6,66 +6,77 @@ describe('Directive: deploymentEvents', function () {
 
     beforeEach(module('cosmoUiApp', 'ngMock', 'gsUiHelper', 'templates-main'));
 
-    describe('Test setup', function() {
-        it ('Injecting required data', inject(function ($compile, $rootScope, $helper) {
-            $helper.addInjects([
-                {
-                    method: 'POST',
-                    url: '/backend/events/_search',
-                    respond: 200
-                }
-            ]);
+    beforeEach(inject(function ($compile, $rootScope, $helper, $httpBackend) {
+        $httpBackend.whenPOST('/backend/events/_search').respond(200);
 
-            scope = $rootScope.$new();
-            element = $compile(angular.element('<div deployment-events></div>'))(scope);
+        scope = $rootScope.$new();
+        element = $compile(angular.element('<div deployment-events></div>'))(scope);
 
-            $rootScope.$apply();
+        $rootScope.$apply();
 
-            scope = element.isolateScope();
-            scope.$apply();
-        }));
-    });
+        scope = element.isolateScope();
+        scope.$apply();
+    }));
 
-    describe('Directive tests', function() {
+    describe('deploymentEvents scope', function () {
 
-        it('should create scope object', function() {
+        it('should create scope object', function () {
             expect(scope).not.toBeUndefined();
         });
 
-        it('should create an element with getEventIcon function', function() {
+        it('should create an element with getEventIcon function', function () {
             expect(typeof(scope.getEventIcon)).toBe('function');
         });
 
-        it('should create an element with getEventText function', function() {
+        it('should create an element with getEventText function', function () {
             expect(typeof(scope.getEventText)).toBe('function');
         });
 
-        it('should create an element with lastEvent function', function() {
+        it('should create an element with lastEvent function', function () {
             expect(typeof(scope.lastEvent)).toBe('function');
         });
 
-        it('should create an element with hasLastEvent function', function() {
+        it('should create an element with hasLastEvent function', function () {
             expect(typeof(scope.hasLastEvent)).toBe('function');
         });
 
-        it('should create an element with dragIt function', function() {
+        it('should create an element with dragIt function', function () {
             expect(typeof(scope.dragIt)).toBe('function');
         });
 
-        it('should have events-widget div', function() {
+        it('should have events-widget div', function () {
             expect(element.find('div.events-widget').length).toBe(1);
         });
 
-        it('should have opacity background div', function() {
+        it('should have opacity background div', function () {
             expect(element.find('div.bg').length).toBe(1);
         });
 
-        it('should have head div', function() {
+        it('should have head div', function () {
             expect(element.find('div.head').length).toBe(1);
         });
 
-        it('should have containList div', function() {
+        it('should have containList div', function () {
             expect(element.find('div.containList').length).toBe(1);
+        });
+
+    });
+
+
+    describe('drag head functionality', function () {
+        it('should change the height', function(){
+            $('body').append(element);
+            $('body').addClass('bpContainer');
+            $('body').attr('id','main-content');
+            var eventsList = element.find('.containList');
+            var dragBtn = element.find('.dragBtn');
+            dragBtn.simulate('drag', { dy:10}); // drag once to enforce maxHeight..
+            var firstHeight = eventsList.height();
+            dragBtn.simulate('drag', { dy:10}); // actual drag.
+            var lastHeight =  eventsList.height();
+            expect(firstHeight - lastHeight).toBe(10);
+            $('body').removeClass('bpContainer');
+            $('body').attr('id',null);
         });
 
     });
