9a1a35962cd161f97cfb7be07240b0bc706d34f7
sefigiga <gigaS3f1nbar>
2015-04-06 10:35:30 +0300

    fix(CFY-2383): change nested nodes badges
    [CFY-2383] fix tests to reflect code changes

--- test/spec/services/nodeStatus.js
+++ test/spec/services/nodeStatus.js
@@ -14,23 +14,43 @@ describe('Service: nodeStatus', function () {
         expect(!!nodeStatus).toBe(true);
     });
 
-    it('should have install status in status id 0', function() {
+    it('should have install status in status id 0', function () {
         expect(nodeStatus.getStatus(0)).toBe('install');
     });
 
-    it('should have done status in status id 1', function() {
+    it('should have done status in status id 1', function () {
         expect(nodeStatus.getStatus(1)).toBe('done');
     });
 
-    it('should have install status in status id 2', function() {
+    it('should have install status in status id 2', function () {
         expect(nodeStatus.getStatus(2)).toBe('install');
     });
 
-    it('should have failed status in status id 3', function() {
+    it('should have failed status in status id 3', function () {
         expect(nodeStatus.getStatus(3)).toBe('failed');
     });
 
-    it('should return empty status as default', function() {
+    it('should return empty status as default', function () {
         expect(nodeStatus.getStatus()).toBe('');
+    });
+
+    it('should return loading icon in status id 0', function () {
+        expect(nodeStatus.getIcon(0)).toBe(' icon-gs-node-status-loading');
+    });
+
+    it('should return success icon in status id 1', function () {
+        expect(nodeStatus.getIcon(1)).toBe(' icon-gs-node-status-success');
+    });
+
+    it('should return loading icon in status id 2', function () {
+        expect(nodeStatus.getIcon(2)).toBe(' icon-gs-node-status-loading');
+    });
+
+    it('should return failed icon in status id 3', function () {
+        expect(nodeStatus.getIcon(3)).toBe(' icon-gs-node-status-fail');
+    });
+
+    it('should return no icon in status as default', function () {
+        expect(nodeStatus.getIcon()).toBe('');
     });
 });
