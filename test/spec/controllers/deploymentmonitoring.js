c02d2b436b7d7f44c8684fd892c58423c7359226
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-04-16 03:38:37 +0300

    improve(test): increase code coverage on controllers

--- test/spec/controllers/deploymentmonitoring.js
+++ test/spec/controllers/deploymentmonitoring.js
@@ -2,21 +2,23 @@
 
 describe('Controller: DeploymentmonitoringCtrl', function () {
 
-//  // load the controller's module
-//  beforeEach(module('cosmoUiAppApp'));
-//
-//  var DeploymentmonitoringCtrl,
-//    scope;
-//
-//  // Initialize the controller and a mock scope
-//  beforeEach(inject(function ($controller, $rootScope) {
-//    scope = $rootScope.$new();
-//    DeploymentmonitoringCtrl = $controller('DeploymentmonitoringCtrl', {
-//      $scope: scope
-//    });
-//  }));
-//
-//  it('should attach a list of awesomeThings to the scope', function () {
-//    expect(scope.awesomeThings.length).toBe(3);
-//  });
+    // load the controller's module
+    beforeEach(module('cosmoUiApp', 'backend-mock'));
+
+    var DeploymentMonitoringCtrl,
+        scope;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        DeploymentMonitoringCtrl = $controller('DeploymentMonitoringCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should attach a list of awesomeThings to the scope', function () {
+        expect(scope.showGrafanaLoader).toBe(true);
+        scope.grafanaLoad();
+        expect(scope.showGrafanaLoader).toBe(false);
+    });
 });
