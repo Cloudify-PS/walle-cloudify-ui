1a79021df624b290098f78a3c5f60b1c6a15c062
erezcarmel <erezcarmel@gmail.com>
2015-06-14 10:45:38 +0300

    CFY-2718 Fixed delete dialog process - PR review comment

--- app/scripts/controllers/deleteDialog.js
+++ app/scripts/controllers/deleteDialog.js
@@ -21,49 +21,52 @@ angular.module('cosmoUiApp')
             $scope.deleteState.inProcess = true;
 
             if(!!$scope.deleteState.itemToDelete) {
-                $scope.deleteState.type = DELETE_TYPES.BLUEPRINT;
-                CloudifyService.blueprints.delete({id: $scope.deleteState.itemToDelete.id})
-                    .then(function(data) {
-                        if (data.error_code !== undefined) {
-                            $scope.deleteState.inProcess = false;
-                            $scope.deleteState.errorMessage = data.message;
-                        } else {
-                            $timeout(function() {
-                                $scope.closeDialog();
-                                $scope.loadBlueprints();
+                switch($scope.deleteState.type) {
+                    case DELETE_TYPES.BLUEPRINT:
+                        CloudifyService.blueprints.delete({id: $scope.deleteState.itemToDelete.id})
+                            .then(function(data) {
+                                if (data.error_code !== undefined) {
+                                    $scope.deleteState.inProcess = false;
+                                    $scope.deleteState.errorMessage = data.message;
+                                } else {
+                                    $timeout(function() {
+                                        $scope.closeDialog();
+                                        $scope.loadBlueprints();
+                                        $scope.deleteState.inProcess = false;
+                                    }, 1000);
+                                }
+                            }, function(e) {
                                 $scope.deleteState.inProcess = false;
-                            }, 1000);
-                        }
-                    }, function(e) {
-                        $scope.deleteState.inProcess = false;
-                        if(e.data.hasOwnProperty('message')) {
-                            $scope.deleteState.errorMessage = e.data.message;
-                        } else {
-                            $scope.deleteState.errorMessage = 'An error occurred';
-                        }
-                    });
-            } else if(!!$scope.deleteState.itemToDelete) {
-                $scope.deleteState.type = DELETE_TYPES.DEPLOYMENT;
-                CloudifyService.deployments.deleteDeploymentById({deployment_id: $scope.deleteState.itemToDelete.id, ignoreLiveNodes: $scope.ignoreLiveNodes})
-                    .then(function(data){
-                        $scope.deleteState.inProcess = false;
-                        if(data.hasOwnProperty('message')) {
-                            $scope.deleteState.errorMessage = data.message;
-                        }
-                        else {
-                            $scope.closeDialog();
-                            $timeout(function(){
-                                $scope.loadDeployments();
-                            }, 500);
-                        }
-                    }, function(e) {
-                        $scope.deleteState.inProcess = false;
-                        if(e.data.hasOwnProperty('message')) {
-                            $scope.deleteState.errorMessage = e.data.message;
-                        } else {
-                            $scope.deleteState.errorMessage = 'An error occurred';
-                        }
-                    });
+                                if(e.data.hasOwnProperty('message')) {
+                                    $scope.deleteState.errorMessage = e.data.message;
+                                } else {
+                                    $scope.deleteState.errorMessage = 'An error occurred';
+                                }
+                            });
+                        break;
+                    case DELETE_TYPES.DEPLOYMENT:
+                        CloudifyService.deployments.deleteDeploymentById({deployment_id: $scope.deleteState.itemToDelete.id, ignoreLiveNodes: $scope.ignoreLiveNodes})
+                            .then(function(data){
+                                $scope.deleteState.inProcess = false;
+                                if(data.hasOwnProperty('message')) {
+                                    $scope.deleteState.errorMessage = data.message;
+                                }
+                                else {
+                                    $scope.closeDialog();
+                                    $timeout(function(){
+                                        $scope.loadDeployments();
+                                    }, 500);
+                                }
+                            }, function(e) {
+                                $scope.deleteState.inProcess = false;
+                                if(e.data.hasOwnProperty('message')) {
+                                    $scope.deleteState.errorMessage = e.data.message;
+                                } else {
+                                    $scope.deleteState.errorMessage = 'An error occurred';
+                                }
+                            });
+                        break;
+                }
             }
         };
 
