374adba894d2238abdb210973b0f1aa49deea3f6
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-06-23 09:11:14 +0300

    feat(cloudifyjs): use cloudifyjs in blueprintsIndex
    before blueprints.list from CloudifyService would also bring deployments. Now we split it to 2 calls.
    
    We only list id,created_at, updated_at for blueprint. So when we want to deploy it, we need to fetch entire blueprint. add logic before dialog opens.
    
    CFY-2896

--- app/scripts/controllers/blueprintsIndex.js
+++ app/scripts/controllers/blueprintsIndex.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('cosmoUiApp')
-    .controller('BlueprintsIndexCtrl', function ($scope, $location, $cookieStore, BreadcrumbsService, $timeout, $log, CloudifyService, ngDialog) {
+    .controller('BlueprintsIndexCtrl', function ($scope, $location, $cookieStore, BreadcrumbsService, $timeout, $log, ngDialog, cloudifyClient) {
         $scope.lastExecutedPlan = null;
         $scope.selectedBlueprint = null;
         $scope.managerError = false;
@@ -33,16 +33,17 @@ angular.module('cosmoUiApp')
         };
 
         $scope.openDeployDialog = function(blueprint) {
-            if (_isDialogOpen()) {
-                return;
-            }
-            $scope.selectedBlueprint = blueprint || null;
             _dialog = ngDialog.open({
                 template: 'views/dialogs/deploy.html',
                 controller: 'DeployDialogCtrl',
                 scope: $scope,
                 className: 'deploy-dialog'
             });
+
+            cloudifyClient.blueprints.get(blueprint.id, null).then(function(result){
+                $scope.selectedBlueprint = result.data || null;
+
+            }); // todo: add error handling
         };
 
         $scope.openDeleteDialog = function() {
@@ -62,22 +63,44 @@ angular.module('cosmoUiApp')
             $scope.openDeleteDialog();
         };
 
-        $scope.loadBlueprints = function() {
+        function loadBlueprints() {
             $scope.blueprints = null;
-            CloudifyService.blueprints.list()
-                .then(function(data) {
-                    $scope.managerError = false;
-                    if (data.length < 1) {
+            $scope.managerError = false;
+            cloudifyClient.blueprints.list('id,updated_at,created_at').then( function( result ){
+
+                    if (result.data.length < 1) {
                         $scope.blueprints = [];
                     } else {
-                        $scope.blueprints = data;
-                        updateDeployments();
+                        $scope.blueprints = _.sortByOrder(result.data, ['updated_at'], [false]);
+
                         $scope.closeDialog();
                     }
+            }, function( result ){
+                $scope.managerError = result.data;
+                $log.error('got error result', result.data);
+            });
+        }
 
-                }, function() {
-                    $scope.managerError = true;
+        // blueprintID to deployment count map
+        var deploymentsCount = {};
+        function loadDeployments(){
+            cloudifyClient.deployments.list('blueprint_id').then(function( result ){
+                _.each(result.data, function( dep ){
+                    $log.info( dep );
+                    if ( !deploymentsCount.hasOwnProperty(dep.blueprint_id)){
+                        deploymentsCount[dep.blueprint_id] = 0;
+                    }
+                    deploymentsCount[dep.blueprint_id]++;
                 });
+            }, function(result){
+                // alert on error somewhere. todo.
+            });
+        }
+
+
+
+        $scope.countDeployments = function(blueprint){
+            return deploymentsCount.hasOwnProperty(blueprint.id) ? deploymentsCount[blueprint.id] : 0;
         };
 
         $scope.uploadDone = function(blueprint_id) {
@@ -103,35 +126,11 @@ angular.module('cosmoUiApp')
             _dialog = null;
         };
 
-        function updateDeployments() {
-            for (var i = 0; i < $scope.blueprints.length; i++) {
-                var index = _getBlueprintArrNextIndex($scope.blueprints[i].id);
-                _blueprintsArr[index].name = $scope.blueprints[i].name;
-            }
-        }
-
-        function _getBlueprintArrNextIndex(blueprint_id) {
-            var nextIndex = -1;
-            for (var j = 0; j < _blueprintsArr.length; j++) {
-                if (_blueprintsArr[j].id === blueprint_id) {
-                    nextIndex = j;
-                }
-            }
-
-            if (nextIndex === -1) {
-                _blueprintsArr.push({
-                    id: blueprint_id,
-                    deployments: []
-                });
-                nextIndex = _blueprintsArr.length - 1;
-            }
-
-            return nextIndex;
-        }
 
         function _isDialogOpen() {
             return _dialog !== null && ngDialog.isOpen(_dialog.id);
         }
 
-        $scope.loadBlueprints();
+        loadBlueprints();
+        loadDeployments();
     });
