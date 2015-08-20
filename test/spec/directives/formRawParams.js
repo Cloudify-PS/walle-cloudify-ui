6d71f2e88c09494617e66d9ef6ee8cb8cb126627
Eden <eden@gigaspaces.com>
2015-08-20 14:11:22 +0300

    fix(formRawParams description as tooltip)
    CFY-3246

--- test/spec/directives/formRawParams.js
+++ test/spec/directives/formRawParams.js
@@ -166,7 +166,7 @@ describe('Directive: formRawParams', function () {
                 translate = $filter('translate');
             }));
 
-            it('tooltip should show input params description', function(){
+            it('should show input params description as tooltip', function(){
                 scope.params = { 'foo' : {'description':'A yummy snickers'}};
                 scope.$digest();
                 content.append(element);
@@ -174,7 +174,7 @@ describe('Directive: formRawParams', function () {
                 expect(inputParameters[0].getAttribute('title')).toBe('A yummy snickers');
             });
 
-            it('tooltip should show no description defined message', function(){
+            it('should show no description defined message as tooltip', function(){
                 scope.params = {'bar' : {}};
                 scope.$digest();
                 content.append(element);
