34db78490150bf63c2eb8ffbee317a863f506064
Eden <eden@gigaspaces.com>
2015-09-02 17:19:00 +0300

    development

--- app/scripts/controllers/logs.js
+++ app/scripts/controllers/logs.js
@@ -27,21 +27,25 @@ angular.module('cosmoUiApp')
         $scope.updateData =function (tableState){
             var options = {
                 from: tableState.pagination.start,
-                size: tableState.pagination.number,
+                size: tableState.pagination.number
                 ///*           query: {},
                 //           filters: [],
                 //*/
                 };
-            if( tableState.sort != {}){
+
+            if( Object.keys(tableState.sort ).length > 0){
                 options.sort ={
                     field: tableState.sort.predicate,
-                    order: tableState.sort.reverse ? 'desc' : 'asc'
+                    order: tableState.sort.reverse ? 'desc' : 'asc',
+                    //field: "timestamp",
+                    //order: 'asc'
                 };
             }
             console.log(tableState);
+            console.log(options);
             EventsService.execute(options, function(error, response){
 
-                console.log(response.data);
+                //console.log(response.data);
                     $scope.logsHits = response.data.hits.hits;
                     tableState.pagination.totalItemCount = response.data.hits.total;
                     tableState.pagination.numberOfPages = Math.ceil(response.data.hits.total/options.size);
--- app/scripts/services/EventsService.js
+++ app/scripts/services/EventsService.js
@@ -4,8 +4,6 @@ angular.module('cosmoUiApp')
     .service('EventsService', function EventsService($http, $q, ejsResource, $log, $rootScope, $filter,cloudifyClient ) {
 
         this.execute = function(options, callBack) {
-            return cloudifyClient.events.query(options, callBack);
-        }
-
-
+            return cloudifyClient.events.get(options, callBack);
+        };
     });
