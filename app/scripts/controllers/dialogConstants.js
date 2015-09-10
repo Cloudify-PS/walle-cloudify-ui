a5bc4e67f03cc1e2537a4fd990ef714541db323e
krzysztof <krzysztof.furmann@gmail.com>
2015-09-10 15:50:40 +0200

    Merge remote-tracking branch 'origin/CFY-3456-add-actions-menu-in-deployment-layout' into CFY-3456-add-actions-menu-in-deployment-layout
    Conflicts:
    	app/index.html
    	app/scripts/controllers/blueprintsIndex.js
    	app/scripts/controllers/deleteDialog.js
    	app/scripts/controllers/deployDialog.js
    	app/scripts/controllers/deployments.js
    	app/scripts/controllers/executeDialog.js
    	app/scripts/directives/blueprintLayout.js
    	app/scripts/directives/bpActionSelector.js
    	app/styles/_actionSelector.scss
    	app/styles/main.scss
    	app/views/blueprint/blueprintLayout.html
    	app/views/blueprintsIndex.html
    	app/views/deployment/deploymentLayout.html
    	app/views/deployments.html
    	app/views/dialogs/executeDialog.html
    	app/views/directives/actionSelector.html
    	test/spec/directives/actionSelector.js

--- app/scripts/controllers/dialogConstants.js
+++ app/scripts/controllers/dialogConstants.js
@@ -3,13 +3,6 @@ angular.module('cosmoUiApp')
         'BLUEPRINT': 'blueprint',
         'DEPLOYMENT': 'deployment'
     })
-    .constant('DIALOG_EVENTS', {
-        'BLUEPRINT_DELETED' : 'blueprint_deleted',
-        'DEPLOYMENT_CREATED' : 'deployment_created',
-        'DEPLOYMENT_DELETED' : 'deployment_deleted',
-        'EXECUTION_STARTED' : 'execution_started',
-        'EXECUTION_CANCELED' : 'execution_canceled'
-    })
     .constant('INPUT_STATE', {
         'PARAMS': 'params',
         'RAW': 'raw'
