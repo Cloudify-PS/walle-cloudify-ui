dac4ef7faa748f72fdb2e9e32a3cfb97392f5400
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-09-20 16:49:36 +0300

    add support to multiple compression types
    CFY-3482

--- test/backend/unit/mocha/services/BrowseBlueprintService.js
+++ test/backend/unit/mocha/services/BrowseBlueprintService.js
@@ -4,6 +4,7 @@ var conf = require('../../../../../backend/appConf');
 var path = require('path');
 var fs = require('fs.extra');
 var BrowseBlueprintService = require('../../../../../backend/services/BrowseBlueprintService');
+var logger = require('log4js').getLogger('BrowseBlueprintService');
 
 
 describe('BrowseBlueprintService', function () {
@@ -14,8 +15,8 @@ describe('BrowseBlueprintService', function () {
 
     afterEach(function () {
         console.log('deleting  ', conf.browseBlueprint.path);
-        fs.removeSync( path.join( conf.browseBlueprint.path, 'invalidBlueprint') );
-        fs.removeSync( path.join( conf.browseBlueprint.path, '111.archive') );
+        fs.removeSync(path.join(conf.browseBlueprint.path, 'invalidBlueprint'));
+        fs.removeSync(path.join(conf.browseBlueprint.path, '111.archive'));
     });
 
     describe('browseBlueprint', function () {
@@ -28,4 +29,40 @@ describe('BrowseBlueprintService', function () {
             });
         });
     });
+});
+
+
+describe.only('compression', function () {
+    var dest = path.join(__dirname, '../../../../../dev/test_resources/tmp');
+
+    beforeEach(function () {
+        fs.removeSync(dest);
+    });
+
+    afterEach(function () {
+        fs.removeSync(dest);
+    });
+
+
+    ['zip', 'tar', 'tar.gz'].forEach(function (type) {
+        it('should decompress ' + type, function (done) {
+            var file = path.join(__dirname, '../../../../resources/compression/test_file.' + type);
+            BrowseBlueprintService.extractArchive(type, file, dest, function (err) {
+                logger.info('callback was called', err);
+                expect(!err).to.be(true);
+
+                function testFileExistance() {
+                    if (!fs.existsSync(path.join(dest, 'test_file.txt'))) {
+                        setTimeout(testFileExistance, 100);
+                    } else {
+                        done();
+                    }
+                }
+
+                testFileExistance();
+
+            });
+        });
+    });
+
 });
