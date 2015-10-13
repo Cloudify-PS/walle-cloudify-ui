1b0b834709ec6f1444a64f3e72771c2af45662c8
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-10-13 11:54:32 +0300

    feature(topology): use the new topology
    CFY-3759

---
+++ app/scripts/controllers/NewTopology.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cosmoUiApp.controller:NewTopologyCtrl
+ * @description
+ * # NewTopologyCtrl
+ * Controller of the cosmoUiApp
+ */
+angular.module('cosmoUiApp')
+    .controller('NewTopologyCtrl', function ($scope, cloudifyClient, $routeParams, DataProcessingService) {
+        var blueprintId = $routeParams.blueprintId;
+
+        $scope.topologyLoading = true;
+        if ( blueprintId ) {
+            cloudifyClient.blueprints.get(blueprintId).then(function (result) {
+                console.log('hello world', result.data);
+
+                var data = result.data;
+
+                var topologyData = {
+                    data: data,
+                    networkBarLocation: 316,
+                    scale: 0.75,
+                    offset: [0, 29]
+                };
+
+                $scope.topologyData = DataProcessingService.encodeTopologyFromRest(topologyData);
+                $scope.topologyLoading = false;
+            });
+        }
+    });
