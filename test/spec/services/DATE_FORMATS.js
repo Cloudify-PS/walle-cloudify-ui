ff6962e37a1d6eeb25415e4ff4dd39c2af76085e
Eden <eden@gigaspaces.com>
2016-03-03 17:50:27 +0200

    refactor(timestamp convertions) added constant to reuse format strings CFY-2433

--- test/spec/services/DATE_FORMATS.js
+++ test/spec/services/DATE_FORMATS.js
@@ -2,20 +2,20 @@
 
 describe('Service: DATE_FORMATS', function () {
 
-  // load the service's module
-  beforeEach(module('cosmoUiApp'));
+    // load the service's module
+    beforeEach(module('cosmoUiApp'));
 
-  // instantiate service
-  var mDATE_FORMATS;
-  beforeEach(inject(function (DATE_FORMATS) {
-    mDATE_FORMATS = DATE_FORMATS;
-  }));
+    // instantiate service
+    var mDATE_FORMATS;
 
-  it('should do something', function () {
-    expect(!!mDATE_FORMATS).toEqual({
-        short: 'yyyy-MM-dd HH:mm:ss',
-        long: 'yyyy-MM-dd HH:mm:ss.sss'
+    beforeEach(inject(function (DATE_FORMATS) {
+        mDATE_FORMATS = DATE_FORMATS;
+    }));
+
+    it('should do something', function () {
+        expect(!!mDATE_FORMATS).toEqual({
+            short: 'yyyy-MM-dd HH:mm:ss',
+            long: 'yyyy-MM-dd HH:mm:ss.sss'
+        });
     });
-  });
-
 });
